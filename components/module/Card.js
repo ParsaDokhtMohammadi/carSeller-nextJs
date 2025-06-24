d3128ac72ddc1b2c990bcf06635ac66a62d1313c
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-06-24 14:48:56 +0330

    card component added

--- components/module/Card.js
+++ components/module/Card.js
@@ -1,26 +1,23 @@
-import Link from "next/link";
-import Location from "../icons/Location";
-import styles from "./Card.module.css";
-
-function Card(props) {
-  const { id, name, model, year, distance, location, image, price } = props;
-
-  return (
-    <Link href={`/cars/${id}`}>
-      <div className={styles.container}>
-        <img src={image} className={styles.image} />
-        <h4 className={styles.title}>{`${name} ${model}`}</h4>
-        <p className={styles.detail}>{`${year} . ${distance}km`}</p>
-        <div className={styles.footer}>
-          <p>$ {price}</p>
-          <div className={styles.location}>
-            <p>{location}</p>
-            <Location />
-          </div>
+import Styles from './Card.module.css'
+import Location from "../icons/Location"
+const Card = (props) => {
+    const {id,name,model,year,distance,location,image,price}=props
+    return (
+    <>
+        <div className={Styles.container}>
+            <img src={image} className={Styles.image}></img>
+            <h4 className={Styles.title}>{`${name} ${model}`}</h4>
+            <p className={Styles.detail}>{`${year} ${distance}km`}</p>
+            <div className={Styles.footer}>
+                <p>${price}</p>
+                <div className={Styles.location}>
+                    <p>{location}</p>
+                    <Location></Location>
+                </div>
+            </div>
         </div>
-      </div>
-    </Link>
-  );
+    </>
+  )
 }
 
-export default Card;
+export default Card
--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,10 +1,11 @@
 import carsData from "@/data/carsData"
 import styles from "../../styles/CarsPage.module.css"
+import Card from "@/components/module/Card"
 const Cars = () => {
   return (
     <div className={styles.container}>
         {carsData.map(car=>(
-            <p key={car.id}>{car.name}</p>
+            <Card key={car.id} {...car}></Card>
         ))}
     </div>
   )
